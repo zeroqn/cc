02c7e9394ce988894aef5f22f04b4c1bd015c246
zero.qn <zero.qn@outlook.com>
2019-07-20 18:19:31 +0800

    chore(secp256k1): use Signature instead of RecoverableSignature
    RecoverableSignature has a problem, it allows modified RecoveryId
    to create another signature and public key and pass verify.

--- src/secp256k1.rs
+++ src/secp256k1.rs
@@ -2,15 +2,9 @@ use crate::error::CryptoError;
 use crate::hash::Hash;
 use crate::traits::{PrivateKey, PublicKey, Signature};
 
-use secp256k1::constants::COMPACT_SIGNATURE_SIZE;
-use secp256k1::recovery::{RecoverableSignature, RecoveryId};
 use secp256k1::{Message, Secp256k1, SignOnly, ThirtyTwoByteHash, VerifyOnly};
 
 use std::convert::TryFrom;
-
-// 1 byte for RecoveryId(0, 1, 2, 3)
-// Reference: https://docs.rs/secp256k1/0.14.1/src/secp256k1/recovery/mod.rs.html#40-45
-const RECOVERABLE_SIGNATURE_SIZE: usize = COMPACT_SIGNATURE_SIZE + 1;
 
 pub struct Secp256k1PrivateKey {
     secret_key: secp256k1::SecretKey,
@@ -23,7 +17,7 @@ pub struct Secp256k1PublicKey {
 }
 
 pub struct Secp256k1Signature {
-    rec_sig: RecoverableSignature,
+    sig: secp256k1::Signature,
     engine: Secp256k1<VerifyOnly>,
 }
 
@@ -48,10 +42,10 @@ impl PrivateKey<32> for Secp256k1PrivateKey {
 
     fn sign_message(&self, msg: &Hash) -> Self::Signature {
         let msg = Message::from(msg);
-        let rec_sig = self.engine.sign_recoverable(&msg, &self.secret_key);
+        let sig = self.engine.sign(&msg, &self.secret_key);
         let engine = Secp256k1::verification_only();
 
-        Secp256k1Signature { rec_sig, engine }
+        Secp256k1Signature { sig, engine }
     }
 
     fn pub_key(&self) -> Self::PublicKey {
@@ -89,9 +83,8 @@ impl PublicKey<33> for Secp256k1PublicKey {
 
     fn verify_signature(&self, msg: &Hash, sig: &Self::Signature) -> Result<(), CryptoError> {
         let msg = Message::from(msg);
-        let sig = sig.rec_sig.to_standard();
 
-        self.engine.verify(&msg, &sig, &self.pub_key)?;
+        self.engine.verify(&msg, &sig.sig, &self.pub_key)?;
         Ok(())
     }
 
@@ -108,45 +101,25 @@ impl TryFrom<&[u8]> for Secp256k1Signature {
     type Error = CryptoError;
 
     fn try_from(bytes: &[u8]) -> Result<Secp256k1Signature, Self::Error> {
-        if bytes.len() != RECOVERABLE_SIGNATURE_SIZE {
-            Err(CryptoError::InvalidLength)?;
-        }
-
-        let recovery_id = {
-            let i32_id = i32::from(bytes[COMPACT_SIGNATURE_SIZE]);
-            RecoveryId::from_i32(i32_id)?
-        };
-
-        let rec_sig =
-            RecoverableSignature::from_compact(&bytes[..COMPACT_SIGNATURE_SIZE], recovery_id)?;
+        let sig = secp256k1::Signature::from_compact(bytes)?;
         let engine = Secp256k1::verification_only();
 
-        Ok(Secp256k1Signature { rec_sig, engine })
+        Ok(Secp256k1Signature { sig, engine })
     }
 }
 
-impl Signature<65> for Secp256k1Signature {
+impl Signature<64> for Secp256k1Signature {
     type PublicKey = Secp256k1PublicKey;
 
     fn verify(&self, msg: &Hash, pub_key: &Self::PublicKey) -> Result<(), CryptoError> {
         let msg = Message::from(msg);
-        let sig = self.rec_sig.to_standard();
 
-        self.engine.verify(&msg, &sig, &pub_key.pub_key)?;
+        self.engine.verify(&msg, &self.sig, &pub_key.pub_key)?;
         Ok(())
     }
 
-    fn to_bytes(&self) -> [u8; 65] {
-        let mut bytes = [0u8; 65];
-        let (rec_id, serialized) = self.rec_sig.serialize_compact();
-
-        let i32_id = rec_id.to_i32();
-        assert!(i32_id >= 0 && i32_id <= 3);
-
-        bytes[..COMPACT_SIGNATURE_SIZE].copy_from_slice(&serialized[..COMPACT_SIGNATURE_SIZE]);
-        bytes[COMPACT_SIGNATURE_SIZE] = i32_id as u8;
-
-        bytes
+    fn to_bytes(&self) -> [u8; 64] {
+        self.sig.serialize_compact()
     }
 }
 
