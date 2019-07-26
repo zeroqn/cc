337416d5e02c3f99e12f7981c6ad19df98d3cc97
zero.qn <zero.qn@outlook.com>
2019-07-26 11:06:50 +0800

    feat: add keccak256 hasher

---
+++ cc-hasher-keccak256/src/lib.rs
@@ -0,0 +1,25 @@
+use cc_hasher::{HashValue, Hasher};
+
+pub struct Keccak256;
+
+impl Hasher for Keccak256 {
+    fn digest(data: &[u8]) -> HashValue {
+        let bytes = tiny_keccak::keccak256(data);
+
+        HashValue::from_bytes_unchecked(bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Keccak256;
+
+    use cc_hasher::Hasher;
+
+    use quickcheck_macros::quickcheck;
+
+    #[quickcheck]
+    fn prop_keccak256_bytes(msg: String) -> bool {
+        Keccak256::digest(msg.as_bytes()).to_bytes() == tiny_keccak::keccak256(msg.as_bytes())
+    }
+}
